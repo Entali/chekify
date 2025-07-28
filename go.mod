93c5f960db44a77c91977fcc52948178789d5d72
Nata Entali <nata.entali@protonmail.com>
2025-07-28 22:49:28 +0300

    update dependencies, improve bot logging and initialization

--- go.mod
+++ go.mod
@@ -4,5 +4,11 @@ go 1.24
 
 require (
 	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.6.0
 	gopkg.in/telebot.v4 v4.0.0-beta.5
+)
+
+require (
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
--- main.go
+++ main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"chekify/bot"
 	"github.com/joho/godotenv"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"os"
 )
 
 func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetLevel(log.InfoLevel)
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	bot.InitTelegramBot(os.Getenv("TELEGRAM_TOKEN"))
+	b := bot.InitTelegramBot(os.Getenv("TELEGRAM_TOKEN"))
+	b.Start()
+
+	log.Info("Бот успішно запущений")
 }
