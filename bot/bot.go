9e374e38928f228616ac7d9ce480c583ca14595f
Nata Entali <nata.entali@protonmail.com>
2025-07-28 22:49:38 +0300

    refactor bot initialization: add menu handlers, keyboard creation logic

--- bot/bot.go
+++ bot/bot.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"fmt"
 	tele "gopkg.in/telebot.v4"
 	"log"
 	"time"
@@ -8,8 +9,9 @@ import (
 
 func InitTelegramBot(token string) (bot *tele.Bot) {
 	telegramBotSettings := tele.Settings{
-		Token:  token,
-		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+		Token:   token,
+		Poller:  &tele.LongPoller{Timeout: 10 * time.Second},
+		Verbose: true,
 	}
 	bot, err := tele.NewBot(telegramBotSettings)
 
@@ -18,14 +20,48 @@ func InitTelegramBot(token string) (bot *tele.Bot) {
 		return
 	}
 
-	handleStart(bot)
-	bot.Start()
+	registerHandlers(bot)
 
 	return bot
+}
+
+func registerHandlers(bot *tele.Bot) {
+	handleStart(bot)
+	handleMenu(bot)
+	bot.Start()
+}
+
+func createMenu() tele.ReplyMarkup {
+	menu := tele.ReplyMarkup{ResizeKeyboard: true}
+
+	addManuallyBtn := menu.Data("📝 Додати вручну", "add_manually", "payload")
+	sendCheckBtn := menu.Data("📸 Надіслати чек", "send_check", "payload")
+	postponedTransactionsBtn := menu.Data("⏳ Відкладені транзакції",
+		"postponed_transactions",
+		"інший_пейлоуд")
+
+	menu.Inline(
+		menu.Row(addManuallyBtn),
+		menu.Row(sendCheckBtn),
+		menu.Row(postponedTransactionsBtn),
+	)
+
+	return menu
 }
 
 func handleStart(bot *tele.Bot) {
 	bot.Handle("/start", func(ctx tele.Context) error {
 		return ctx.Send("Вітаю. Я твій розумник для обліку витрат")
+	})
+}
+
+func handleMenu(bot *tele.Bot) {
+	fmt.Println("Run menu")
+	bot.Handle("/menu", func(ctx tele.Context) error {
+		menu := createMenu()
+
+		return ctx.Send("Що хочеш зробити?", &tele.SendOptions{
+			ReplyMarkup: &menu,
+		})
 	})
 }
